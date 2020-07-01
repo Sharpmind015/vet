e18234c51737a4169fed73d40d29ea3477b13165
Akindeji <akindejioluwaseun015@gmail.com>
2020-07-01 06:44:50 -0700

    added effects to the page

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ const nav = document.querySelector('.navbar'),
       burger = document.querySelector('.burger-menu'),
       navbar = document.querySelector('.navbar-nav'),
       navbar2 = document.querySelector('.navbar-collapse');
-
+ 
+ //Animate on Scroll 
+AOS.init({
+       easing: 'ease-in-out-sine',
+       offset: 120,
+       duration: 500
+    });;
 
 // Listen for scrolling event on the browser
 window.addEventListener('scroll', (e) => {
@@ -11,20 +17,19 @@ window.addEventListener('scroll', (e) => {
   // Checks if 100px of the page has been scrolled
   if(offset > 100) {
     nav.classList.add('nav-scroll');
-    // mainNav.classList.add('main-nav-scroll');
   }
   else {
     nav.classList.remove('nav-scroll');
-    // mainNav.classList.remove('main-nav-scroll');
   }
 })
-
+//Event listener for links
 nav.addEventListener('click', (e) => {
   if(e.target.className === 'nav-link') {
     navbar2.classList.toggle('show');
     burger.classList.toggle('toggle');
   }
 })
+//Event listener for menu
 burger.addEventListener('click', (e) => {
     burger.classList.toggle('toggle');
 })
